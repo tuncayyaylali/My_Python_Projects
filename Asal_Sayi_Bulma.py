c0df0000c40945dc36b63597d5662bc52c5d1f12
Tuncay YAYLALI <tuncay_yaylali@yahoo.com>
2022-04-13 09:44:27 +0300

    Hacker Challenge Find a String

---
+++ Asal_Sayi_Bulma.py
@@ -0,0 +1,19 @@
+def asalsayi(num):
+    num = int(num)
+    if num <= 3:
+        return True
+    sayi = 0
+    liste=[]
+    for i in range(1, num):
+        while i<num+1:
+            if num%i == 0:
+                sayi +=1
+                liste.append(i)
+            i +=1
+        print(liste)
+        if sayi == 2:
+            return True
+        else:
+            return False
+
+print(asalsayi(input()))
